7e990c0a0ca626efeb1e74bb23ef48cfa3c0e359
Elvern Neylmav Tanny <elvernneylmav@gmail.com>
2025-01-20 23:41:45 +0800

    update

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 import streamlit as st
+from streamlit_extras.mention import mention
 
 def main():
-    st.title("KJV Bible Analysis and Visualization ✝️")
+    st.title("Bible Analysis and Visualization ✝️")
     st.write("""
         #### Work in Progress...
         Elvern Neylmav T is currently working on this page. Please check back later for updates. :smile:
     """)
+    # st.write("Please refer here for the code implementation in jupter notebook: [kjv.ipynb]")
+    # mention(
+    #     label="verneylmavt/st-kjv-vis/training/kjv.ipynb",
+    #     icon="github",
+    #     url="https://github.com/verneylmavt/st-kjv-vis/training/kjv.ipynb"
+    # )
     # st.image("https://via.placeholder.com/400x200.png?text=Coming+Soon", use_column_width=True)
 
 if __name__ == "__main__":
