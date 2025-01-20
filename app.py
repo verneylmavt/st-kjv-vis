c6093efc1dac87bd47997dc087d6aa43287c178f
Elvern Neylmav Tanny <elvernneylmav@gmail.com>
2025-01-20 23:45:20 +0800

    update

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from streamlit_extras.mention import mention
+# from streamlit_extras.mention import mention
 
 def main():
     st.title("Bible Analysis and Visualization ✝️")
